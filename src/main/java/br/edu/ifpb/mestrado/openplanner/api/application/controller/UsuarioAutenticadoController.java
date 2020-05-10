608cf0582d5e5bb70ceab76c6aebd4fa22976df8
Fagner Lima <contato@fagnerlima.pro.br>
2020-05-10 11:50:09 -0300

    Atualiza endpoint GET /me

--- src/main/java/br/edu/ifpb/mestrado/openplanner/api/application/controller/UsuarioAutenticadoController.java
+++ src/main/java/br/edu/ifpb/mestrado/openplanner/api/application/controller/UsuarioAutenticadoController.java
@@ -11,13 +11,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.edu.ifpb.mestrado.openplanner.api.application.facade.ResponseEntityFacade;
-import br.edu.ifpb.mestrado.openplanner.api.application.factory.GrupoLinkFactory;
 import br.edu.ifpb.mestrado.openplanner.api.application.factory.UsuarioLinkFactory;
 import br.edu.ifpb.mestrado.openplanner.api.domain.model.usuario.Usuario;
 import br.edu.ifpb.mestrado.openplanner.api.domain.service.UsuarioService;
 import br.edu.ifpb.mestrado.openplanner.api.infrastructure.facade.ModelMapperFacade;
 import br.edu.ifpb.mestrado.openplanner.api.presentation.dto.usuario.UsuarioAutenticadoRequestTO;
-import br.edu.ifpb.mestrado.openplanner.api.presentation.dto.usuario.UsuarioResponseTO;
+import br.edu.ifpb.mestrado.openplanner.api.presentation.dto.usuario.UsuarioAutenticadoResponseTO;
 import br.edu.ifpb.mestrado.openplanner.api.presentation.dto.usuario.UsuarioSenhaRequestTO;
 
 @RestController
@@ -34,9 +33,9 @@ public class UsuarioAutenticadoController {
     }
 
     @GetMapping
-    public ResponseEntity<UsuarioResponseTO> find() {
+    public ResponseEntity<UsuarioAutenticadoResponseTO> find() {
         Usuario usuario = usuarioService.getAutenticado();
-        UsuarioResponseTO responseTO = converterService.map(usuario, UsuarioResponseTO.class);
+        UsuarioAutenticadoResponseTO responseTO = converterService.map(usuario, UsuarioAutenticadoResponseTO.class);
 
         addLinks(responseTO);
 
@@ -44,10 +43,10 @@ public class UsuarioAutenticadoController {
     }
 
     @PutMapping
-    public ResponseEntity<UsuarioResponseTO> update(@RequestBody UsuarioAutenticadoRequestTO requestTO) {
+    public ResponseEntity<UsuarioAutenticadoResponseTO> update(@RequestBody UsuarioAutenticadoRequestTO requestTO) {
         Usuario usuario = converterService.map(requestTO, Usuario.class);
         Usuario updatedUsuario = usuarioService.updateAutenticado(usuario);
-        UsuarioResponseTO responseTO = converterService.map(updatedUsuario, UsuarioResponseTO.class);
+        UsuarioAutenticadoResponseTO responseTO = converterService.map(updatedUsuario, UsuarioAutenticadoResponseTO.class);
 
         addLinks(responseTO);
 
@@ -60,9 +59,8 @@ public class UsuarioAutenticadoController {
         usuarioService.updateSenhaAutenticado(requestTO.getSenhaAtual(), requestTO.getSenhaNova());
     }
 
-    private void addLinks(UsuarioResponseTO responseTO) {
+    private void addLinks(UsuarioAutenticadoResponseTO responseTO) {
         responseTO.add(UsuarioLinkFactory.create(responseTO.getId()));
-        responseTO.getGrupos().stream().forEach(grupo -> grupo.add(GrupoLinkFactory.create(grupo.getId())));
     }
 
 }
---
+++ src/main/java/br/edu/ifpb/mestrado/openplanner/api/presentation/dto/usuario/UsuarioAutenticadoResponseTO.java
@@ -0,0 +1,60 @@
+package br.edu.ifpb.mestrado.openplanner.api.presentation.dto.usuario;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import org.springframework.hateoas.RepresentationModel;
+
+public class UsuarioAutenticadoResponseTO extends RepresentationModel<UsuarioAutenticadoResponseTO> implements Serializable {
+
+    private static final long serialVersionUID = -2629152547405312072L;
+
+    private Long id;
+
+    private String nome;
+
+    private LocalDate dataNascimento;
+
+    private String email;
+
+    public UsuarioAutenticadoResponseTO() {
+        super();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("UsuarioAutenticadoResponseTO [id=%s, nome=%s, dataNascimento=%s, email=%s]", id, nome, dataNascimento, email);
+    }
+}
