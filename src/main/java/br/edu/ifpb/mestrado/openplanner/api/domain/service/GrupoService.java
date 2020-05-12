05badb43e5c594b0cbca9766dd4f677ed9b407ce
Fagner Lima <contato@fagnerlima.pro.br>
2020-05-12 20:04:41 -0300

    Remove arquivos depreciados do model Grupo

--- src/main/java/br/edu/ifpb/mestrado/openplanner/api/domain/service/GrupoService.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ifpb.mestrado.openplanner.api.domain.service;
-
-import br.edu.ifpb.mestrado.openplanner.api.domain.model.grupo.Grupo;
-
-public interface GrupoService extends BaseService<Grupo> {
-
-}
--- src/main/java/br/edu/ifpb/mestrado/openplanner/api/infrastructure/persistence/hibernate/repository/GrupoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ifpb.mestrado.openplanner.api.infrastructure.persistence.hibernate.repository;
-
-import br.edu.ifpb.mestrado.openplanner.api.domain.model.grupo.Grupo;
-
-public interface GrupoRepository extends BaseRepository<Grupo> {
-
-}
