dae098c0c63cf8e2827aecfb81e97494fc9cf8c7
Fagner Lima <contato@fagnerlima.pro.br>
2020-05-12 20:04:06 -0300

    Refatora migrations removendo tabela auth.grupo

---
+++ src/main/resources/db/migration/V002__ddl_auth.sql
@@ -0,0 +1,49 @@
+CREATE EXTENSION unaccent;
+
+CREATE SCHEMA auth;
+
+CREATE TABLE auth.usuario (
+  id SERIAL,
+  nome VARCHAR(128) NOT NULL,
+  email VARCHAR(128) NOT NULL,
+  data_nascimento DATE NOT NULL,
+  valor_senha VARCHAR(64),
+  reset_token_senha VARCHAR(255),
+  tentativas_erro_senha SMALLINT,
+  data_atualizacao_senha DATE,
+  ativacao_token VARCHAR(255),
+  pendente BOOLEAN NOT NULL DEFAULT TRUE,
+  bloqueado BOOLEAN NOT NULL DEFAULT FALSE,
+  excluded BOOLEAN NOT NULL DEFAULT FALSE,
+  excluded_at DATE,
+  created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  created_by VARCHAR(32) NOT NULL,
+  updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  updated_by VARCHAR(32) NOT NULL,
+  version INTEGER NOT NULL DEFAULT 0,
+  CONSTRAINT pk_usuario PRIMARY KEY(id),
+  CONSTRAINT uk_usuario_email UNIQUE(email)
+);
+
+CREATE INDEX idx_usuario_excluido ON auth.usuario(excluido);
+
+CREATE TABLE auth.permissao (
+  id SERIAL,
+  papel VARCHAR(64) NOT NULL,
+  descricao VARCHAR(128) NOT NULL,
+  excluded BOOLEAN NOT NULL DEFAULT FALSE,
+  excluded_at DATE,
+  version INTEGER NOT NULL DEFAULT 0,
+  CONSTRAINT pk_permissao PRIMARY KEY(id),
+  CONSTRAINT uk_permissao_papel UNIQUE(papel)
+);
+
+CREATE TABLE auth.usuario_permissao (
+  id_usuario INTEGER NOT NULL,
+  id_permissao INTEGER NOT NULL,
+  CONSTRAINT pk_usuario_permissao PRIMARY KEY(id_usuario, id_permissao),
+  CONSTRAINT fk_usuario_permissao_id_usuario FOREIGN KEY(id_usuario) REFERENCES auth.usuario(id)
+    MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE,
+  CONSTRAINT fk_usuario_permissao_id_permissao FOREIGN KEY(id_permissao) REFERENCES auth.permissao(id)
+    MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE
+);
--- src/main/resources/db/migration/V003__dml_auth.sql
+++ src/main/resources/db/migration/V003__dml_auth.sql
@@ -4,32 +4,23 @@ DECLARE
   _id_system INT := -2;
   _id_admin INT := 1;
 BEGIN
-  -- Usuário e Grupo Administrador
+  -- Usuários Padrão
   INSERT INTO auth.usuario (id, nome, data_nascimento, email, valor_senha, tentativas_erro_senha, data_atualizacao_senha, pendente, bloqueado, created_at, created_by, updated_at, updated_by) VALUES
     (_id_root, 'Super User', '1991-01-01', 'root@email.com', '$2a$10$ruzUEX3kN/b0URwAioSWXOQ5FPeK8LttYNveZywgiFQfq0wI0EVK6', 0, NOW(), FALSE, FALSE, NOW(), 'root', NOW(), 'root'),
     (_id_system, 'System User', '1991-01-01', 'system@email.com', '$2a$10$2hCHsFoN3kyZNNnDwb4e9.5tGGs2tmCh46YKiCRVn18ghDMDgIMZm', 0, NOW(), FALSE, FALSE, NOW(), 'root', NOW(), 'root'),
     (_id_admin, 'Administrador', '1991-01-01', 'admin@email.com', '$2a$10$9.vsponXOmbP6L9RqeZPBeVhojwg4bhjLdVTP/EkLaZGht9juv5fq', 0, NOW(), FALSE, FALSE, NOW(), 'root', NOW(), 'root');
-  INSERT INTO auth.grupo (id, nome, created_at, created_by, updated_at, updated_by) VALUES
-    (_id_root, 'Root', NOW(), 'root', NOW(), 'root'),
-    (_id_system, 'System', NOW(), 'root', NOW(), 'root'),
-    (_id_admin, 'Administrador', NOW(), 'root', NOW(), 'root');
-  INSERT INTO auth.usuario_grupo (id_usuario, id_grupo) VALUES
-    (_id_root, _id_root),
-    (_id_system, _id_system),
-    (_id_admin, _id_admin);
 
   ALTER SEQUENCE auth.usuario_id_seq RESTART WITH 2;
-  ALTER SEQUENCE auth.grupo_id_seq RESTART WITH 2;
 
   -- Permissões
   INSERT INTO auth.permissao (id, papel, descricao) VALUES
-    (_id_root, 'ROLE_ROOT', 'Root'),
-    (_id_system, 'ROLE_SYSTEM', 'System'),
-    (_id_admin, 'ROLE_ADMIN', 'Administrador');
+    (_id_root, 'ROOT', 'Root'),
+    (_id_system, 'SYSTEM', 'System'),
+    (_id_admin, 'ADMIN', 'Administrador');
 
   ALTER SEQUENCE auth.permissao_id_seq RESTART WITH 2;
 
-  INSERT INTO auth.grupo_permissao (id_grupo, id_permissao) VALUES
+  INSERT INTO auth.usuario_permissao (id_usuario, id_permissao) VALUES
     (_id_root, _id_root),
     (_id_system, _id_system),
     (_id_admin, _id_admin);
