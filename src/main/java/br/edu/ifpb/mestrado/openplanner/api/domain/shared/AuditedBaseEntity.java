a81e552462e697df2b51bfb972abb9f8a8dd8775
Fagner Lima <contato@fagnerlima.pro.br>
2020-05-12 20:08:55 -0300

    Remove dependência hibernate-envers

--- src/main/java/br/edu/ifpb/mestrado/openplanner/api/domain/shared/AuditedBaseEntity.java
+++ src/main/java/br/edu/ifpb/mestrado/openplanner/api/domain/shared/AuditedBaseEntity.java
@@ -6,16 +6,12 @@ import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 
-import org.hibernate.envers.AuditOverride;
-import org.hibernate.envers.Audited;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@AuditOverride(forClass = BaseEntity.class)
-@Audited
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public abstract class AuditedBaseEntity extends BaseEntity {
