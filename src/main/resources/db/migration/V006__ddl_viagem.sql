56ed280ccd82b92ca75ac5ef3dcb3e6ecf57380a
Fagner Lima <contato@fagnerlima.pro.br>
2020-05-18 17:53:50 -0300

    Implementa migration ddl_viagem

---
+++ src/main/resources/db/migration/V006__ddl_viagem.sql
@@ -0,0 +1,22 @@
+CREATE TABLE planner.viagem (
+  id SERIAL,
+  id_usuario INT NOT NULL,
+  tipo VARCHAR(32) NOT NULL,
+  data DATE NOT NULL,
+  destino VARCHAR(100) NOT NULL,
+  status VARCHAR(32) NOT NULL,
+  anotacoes TEXT,
+  excluded BOOLEAN NOT NULL DEFAULT FALSE,
+  excluded_at DATE,
+  created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  created_by VARCHAR(128) NOT NULL,
+  updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+  updated_by VARCHAR(128) NOT NULL,
+  version INTEGER NOT NULL DEFAULT 0,
+  CONSTRAINT pk_viagem PRIMARY KEY(id),
+  CONSTRAINT fk_viagem_id_usuario FOREIGN KEY (id_usuario) REFERENCES auth.usuario(id)
+    MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE INDEX idx_viagem_id_usuario ON planner.viagem(id_usuario);
+CREATE INDEX idx_viagem_excluded ON planner.viagem(excluded);
